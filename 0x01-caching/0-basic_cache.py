b69ca95044a75f18feb926c7707f2fdbc89afca0
James Koduah <jameskoduah171@gmail.com>
2023-10-25 01:31:57 +0000

    Initialized repo

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Implementing a basic caching system
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A Basic caching system with no limit"""
+    
