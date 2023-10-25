e5d5608995ddaba0708464fa2880057284c74cfd
James Koduah <jameskoduah171@gmail.com>
2023-10-25 01:44:06 +0000

    0-basic_cache.py: first

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,4 +7,14 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """A Basic caching system with no limit"""
-    
+    def __init__(self):
+        super(BasicCache, self).__init__()
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        self.cache_data.get(key)
