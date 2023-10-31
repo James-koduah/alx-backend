b6194c7e09389feb3cb710371efed2ca68496354
James Koduah <jameskoduah171@gmail.com>
2023-10-31 06:26:31 +0000

    task 5: first

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+basic flask app
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Get a user from the mock database"""
+    id = request.args.get('login_as')
+    if id:
+        try:
+            user = users.get(int(id))
+            return user
+        except Exception as e:
+            return None
+    return None
+
+
+@app.before_request
+def before():
+    """Executed before all other functions"""
+    g.user = get_user()
+
+
+class Config(object):
+    """Config class for our app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello():
+    """A route that returns a welcome string"""
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Select language locale"""
+    language = request.args.get('locale')
+    if language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
