8559177b76bce52f183e0816bfc12a27d7f70056
James Koduah <jameskoduah171@gmail.com>
2023-10-31 05:48:47 +0000

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+basic flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for our app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """Select language locale"""
+    language = request.args.get('locale')
+    if language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """A route that returns a welcome string"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
