495c87e13c3ad6e87d6168a1ee8a1fd6be8f6ce5
James Koduah <jameskoduah171@gmail.com>
2023-10-31 04:59:02 +0000

    1-app.py: sixth: worng languages

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,7 +12,7 @@ babel = Babel(app)
 
 class Config(object):
     """Config class for our app"""
-    LANGUAGES = ['en', 'es']
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
