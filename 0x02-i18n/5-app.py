108dc3e094364660c1b009f6cc98ca7400a593e1
James Koduah <jameskoduah171@gmail.com>
2023-11-03 11:36:03 +0000

    task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -16,6 +16,31 @@ users = {
 }
 
 
+class Config(object):
+    """Config class for our app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello():
+    """A route that returns a welcome string"""
+    return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Select language locale"""
+    language = request.args.get('locale')
+    if language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 def get_user():
     """Get a user from the mock database"""
     id = request.args.get('login_as')
@@ -33,30 +58,6 @@ def before():
     """Executed before all other functions"""
     g.user = get_user()
 
-
-class Config(object):
-    """Config class for our app"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-
-@app.route("/")
-def hello():
-    """A route that returns a welcome string"""
-    return render_template('4-index.html')
-
-
-@babel.localeselector
-def get_locale():
-    """Select language locale"""
-    language = request.args.get('locale')
-    if language in app.config['LANGUAGES']:
-        return language
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
